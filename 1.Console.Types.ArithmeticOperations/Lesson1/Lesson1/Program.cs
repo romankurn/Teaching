3d039dcc1ba3bb680e87c21852569ac4e1f23e72
romankurn <89409920+romankurn@users.noreply.github.com>
2021-08-29 19:32:41 +0300

    Topic 1 (#7)
    * Lesson1
    
    * Funcs 6, 7, 8, 11, 12, 14 completed
    
    * Change in Func11
    
    * Change in 12
    
    * Change in Func14
    
    * Easy solution of func14
    
    Co-authored-by: dseregin <dseregin@1worldsync.com>

--- 1.Console.Types.ArithmeticOperations/Lesson1/Lesson1/Program.cs
+++ 1.Console.Types.ArithmeticOperations/Lesson1/Lesson1/Program.cs
@@ -4,9 +4,55 @@ namespace Lesson1
 {
 	class Program
 	{
+        #region basic things
+        private static void BasicThings()
+        {
+			bool a1 = true; //false;								!!!
+
+			byte a2 = 55; //2^8 [0-255]								!
+			sbyte a3 = 5;// [-128; 127]
+			int a4; //negative: 2^16; 0, positive: (2^16) - 1		!!!
+			long a5; // 2^32: negative, 0, 2^32 - 1 : positive		!!!
+
+			float a6; //											!
+			double a7; //											!!!
+			decimal a8; //											!!
+
+			char ch = '\n'; //enter									!!
+			string str; //											!!!!
+
+			Console.ReadLine(); // ввод строки с клавиатуры
+			Console.WriteLine("чето"); // вывод чето на консоль 
+			Convert.ToInt32("число");  // преобразовать строку в число
+									   // $"{выполняемый блок кода}"
+
+			int num;
+			num = 10;
+			//var number; //НЕЗЯ
+			//number = 10;
+
+			var doubl1 = 1.234;
+			var str2 = "sdfsdf";
+			var bl = true;
+			var ch1 = 'f';
+
+		}
+
+		private static void ArithmeticOperations()
+        {
+			// + - сумма
+			// - - разность
+			// * - умноежение
+			// / - деление
+			// ++ - увеличить значение переменной на 1: а++
+			// -- - уменьшить значение переменной на 1: а--
+			// % - получить остаток от деления: 10%4=2; 10%5=0;
+		}
+		#endregion
+
 		static void Main(string[] args)
 		{
-			
+			Func14();
 		}
 
 		/// <summary>
@@ -15,7 +61,9 @@ namespace Lesson1
 		/// </summary>
 		private static void Func1()
 		{
-
+			Console.Write("Введите число ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"Вы ввели число {a}.");
 		}
 
 		/// <summary>
@@ -23,7 +71,11 @@ namespace Lesson1
 		/// </summary>
 		private static void Func2()
 		{
+			byte a = 5;
+			byte b = 12;
+			int c = -25;
 
+			Console.WriteLine($"{a}  {b}  {c}");
 		}
 
 		/// <summary>
@@ -31,6 +83,12 @@ namespace Lesson1
 		/// </summary>
 		private static void Func3()
 		{
+			byte a = 6;
+			byte b = 8;
+			int c = 54;
+			double g = -51.83;
+
+			Console.WriteLine($"{a}\n{b}\n{c}\n{g}");
 
 		}
 
@@ -39,7 +97,10 @@ namespace Lesson1
 		/// </summary>
 		private static void Func4()
 		{
-
+			Console.Write("Введите число ");
+			var x = Convert.ToInt32(Console.ReadLine());
+			var y = 7 * x * x + 3 * x + 6;
+			Console.WriteLine($"Y = {y}.");
 		}
 
 		/// <summary>
@@ -47,7 +108,10 @@ namespace Lesson1
 		/// </summary>
 		private static void Func5()
 		{
-
+			Console.Write("Введите число ");
+			var x = Convert.ToDouble(Console.ReadLine());
+			var y = 7 * x * x + x / 3 + 6;
+			Console.WriteLine($"Y = {y}.");
 		}
 
 		/// <summary>
@@ -57,16 +121,31 @@ namespace Lesson1
 		/// </summary>
 		private static void Func6()
 		{
-
+			Console.WriteLine("Введите число a");
+			var a = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите число b");
+			var b = Convert.ToDouble(Console.ReadLine());
+			
+			var aver = (a + b) / 2;
+			Console.WriteLine($"Среднее арифметическое a и b = {aver}.");
+			
+			var geom = Math.Sqrt(a * b);
+			Console.WriteLine($"Среднее геометрическое a и b = {geom}.");
 		}
 
 		/// <summary>
 		/// Составить программу решения линейного уравнения ax+b=0
-		/// при целочисленных a и b
+		/// При любых (как целых так и вещественных) а и b, вводимых с клавиатуры
 		/// </summary>
 		private static void Func7()
 		{
-
+			Console.WriteLine("Введите число a");
+			var a = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите число b");
+			var b = Convert.ToDouble(Console.ReadLine());
+			
+			var x = -b / a;
+			Console.WriteLine($"Корень уровнения ax+b=0 равен {x}");			
 		}
 
 		/// <summary>
@@ -80,7 +159,27 @@ namespace Lesson1
 		/// </summary>
 		private static void Func8()
 		{
+			Console.WriteLine("Введите число a");
+			var a = Convert.ToDouble(Console.ReadLine());
+					
+			var a2 = a * a;
+			var a4 = a2 * a2;
+			Console.WriteLine($"a) a4 = {a4}");
 
+			var a6 = a2 * a2 * a2;
+			Console.WriteLine($"б) a6 = {a6}");
+
+			var a7 = a2 * a2 * a2 * a;
+			Console.WriteLine($"в) a7 = {a7}");
+
+			var a8 = a4 * a4;
+			Console.WriteLine($"г) a8 = {a8}");
+
+			var a9 = a4 * a4 * a;
+			Console.WriteLine($"д) a9 = {a9}");
+
+			var a10 = a4 * a4 * a2;
+			Console.WriteLine($"e) a10 = {a10}");
 		}
 
 		/// <summary>
@@ -92,7 +191,12 @@ namespace Lesson1
 		/// </summary>
 		private static void Func10()
 		{
-
+			var number = Convert.ToByte(Console.ReadLine()); //45: 4,5,9,20
+			var tens = number / 10;
+			var units = number % 10;
+			var sum = tens + units;
+			var mult = tens * units;
+			Console.WriteLine($"a {tens}\nb {units}\nc {sum}\nd {mult}");
 		}
 
 		/// <summary>
@@ -100,6 +204,15 @@ namespace Lesson1
 		/// </summary>
 		private static void Func11()
 		{
+			Console.WriteLine("Введите трёхзначное число a");
+			var a = Convert.ToInt32(Console.ReadLine());
+			
+			var hund = a / 100;
+			var tens = (a % 100) / 10;
+			var units = a % 10;
+			var conv = units * 100 + tens * 10 + hund;
+
+			Console.WriteLine($"Прочтение цифр числа а справа налево {conv}");
 
 		}
 
@@ -108,17 +221,14 @@ namespace Lesson1
 		/// </summary>
 		private static void Func12()
 		{
+			Console.WriteLine("Введите трёхзначное число a");
+			var a = Convert.ToInt32(Console.ReadLine());
 
-		}
+			var char1 = a / 100;
+			var char2_3 = a % 100;
+			var conv = char2_3 * 10 + char1;
 
-		/// <summary>
-		/// Дано натуральное число n (n > 9). Найти:
-		/// а) число единиц в нем;
-		/// б) число десятков в нем.
-		/// </summary>
-		private static void Func13()
-		{
-
+			Console.WriteLine($"Полученное число {conv}");
 		}
 
 		/// <summary>
@@ -127,6 +237,7 @@ namespace Lesson1
 		/// </summary>
 		private static void Func15()
 		{
+			//TODO: после логических операций
 		}
 
 		#region Уух сложна
@@ -139,6 +250,15 @@ namespace Lesson1
 		/// </summary>
 		private static void Func9()
 		{
+			//7:23:59 = 26639;
+			var n = Convert.ToInt32(Console.ReadLine());
+
+			var hour = n / 3600;
+			var min = (n % 3600) / 60;
+			var sec = n % 60;
+
+			Console.WriteLine($"a) {hour}\nб) {min}\nв) {sec}");
+
 
 		}
 
@@ -148,7 +268,24 @@ namespace Lesson1
 		/// </summary>
 		private static void Func14()
 		{
+			Console.WriteLine("Введите на сколько градусов у повернулась часовая стрелка");
+			var y = Convert.ToDouble(Console.ReadLine());
 
+			var A = ((y - (y % 30)) / 30); // считает количество прошедших часов
+			int B = Convert.ToInt32(A); // избавляется от Double, чтобы делить без остатков в Int
+			int hour = B - ((B / 24) * 24);
+			var a = ((y - (y % 0.5)) / 0.5); // считает количество прошедших минут
+			var b = Convert.ToInt32(a); // избавляется от Double, чтобы делить без остатков в Int
+			var min = b - ((b / 60) * 60);
+
+			Console.WriteLine($"Число полных часов {hour}\nЧисло полных минут {min}");
+
+			var totalMinutes = (int) (y * 2);
+			var totalHours = totalMinutes / 60;
+			var hours = totalHours % 24;
+			var minutes = totalMinutes % 60;
+			
+			Console.WriteLine($"{hours}:{minutes}");
 		}
 
 		#endregion
