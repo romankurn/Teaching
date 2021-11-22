5709b337d5068006f0e0c56abb935847ae7bb278
romankurn <89409920+romankurn@users.noreply.github.com>
2021-11-22 17:22:52 +0300

    books

---
+++ 7.ООП.Наследование/Inherit/Inherit/Homework/BookGenrePubl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit
+{
+	public class BookGenrePubl : BookGenre
+	{
+		protected string _publ;
+
+		public string Publ 
+		{ 
+			get 
+			{ 
+				return _publ;
+			} 
+			private set 
+			{ 
+				_publ = value; 
+			} 
+		}
+
+		public BookGenrePubl (string title, string authorsSurnameAndFirstName, double price, string genre, string publ) : base(title,authorsSurnameAndFirstName, price, genre)
+		{
+			Publ = publ;
+		}
+
+		public void Print()
+		{
+			base.Print();
+			Console.Write($", book publisher: {Publ}");
+		}
+	}
+}
