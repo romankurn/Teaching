e09acc339e1d46a06238cfef0b8da6831ad2b355
romankurn <89409920+romankurn@users.noreply.github.com>
2021-11-25 18:08:23 +0300

    posdpgjmdfg

---
+++ 9.ООП.Полиморфизм/Polymorphism/Polymorphism/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Polymorphism
+{
+	public abstract class Animal
+	{
+		public virtual void Move()
+		{
+			Console.WriteLine("Animal moves");
+		}
+
+		public virtual void Say()
+		{
+			Console.WriteLine("Animal says");
+		}
+	}
+}
---
+++ 9.ООП.Полиморфизм/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Polymorphism
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Animal bird = new Bird();
+			bird.Say(); // Animal says
+
+			var mammal = new Mammal();
+			mammal.Say(); // Mammal says
+		}
+
+		static void MakeAnimalSay(Animal animal)
+		{
+			animal.Say();
+		}
+	}
+}
