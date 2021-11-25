b530611b094ea9cac6dbeb3c63183611b0cff02e
romankurn <89409920+romankurn@users.noreply.github.com>
2021-11-25 19:43:43 +0300

    Polimorphism (#37)
    * posdpgjmdfg
    
    * вап

---
+++ 9.ООП.Полиморфизм/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Polymorphism
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var student1 = new Student("Bob", 20, "JD", 2);
+			var student2 = new Student("Tom", 21, "BF", 3);
+			var teacher = new Teacher("Tim", 52, "hg", 4);
+
+
+		}
+
+		static void Introduse(Person person)
+		{
+			person.Print();
+		}
+
+		static Person GetCopy(Person person)
+		{
+			return person.Clone();
+		}
+	}
+}
---
+++ 9.ООП.Полиморфизм/Polymorphism/Polymorphism/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+	public class Teacher : Student
+	{
+		public List<Guid> StudentIds { get; set; } = new List<Guid>();
+
+		public Teacher(string name, int age, string facility, int course) : base(name, age, facility, course)
+		{
+
+		}
+
+		public override void Print()
+		{
+			Console.WriteLine($"{GetType().Name}. Name: {Name}, Age: {Age}");
+		}
+
+		public override Person Clone()
+		{
+			return new Teacher(Name, Age, Facility, Course) { Id = Id, StudentIds = StudentIds.Select(id => id).ToList()};
+		}
+	}
+}
