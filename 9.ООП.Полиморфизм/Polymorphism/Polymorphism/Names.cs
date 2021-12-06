550a5bf6b7437129aae40fb2fd9a7ddb3fcd4e65
romankurn <89409920+romankurn@users.noreply.github.com>
2021-12-06 18:38:12 +0300

    pol homework

---
+++ 9.ООП.Полиморфизм/Polymorphism/Polymorphism/Names.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Polymorphism
+{
+	public class Names
+	{
+		public static List<string> RandomNames { get; set; } = new List<string>()
+		{
+		"RandomName1",
+		"RandomName2",
+		"RandomName3",
+		"RandomName4",
+		"RandomName5",
+		"RandomName6",
+		"RandomName7",
+		"RandomName8",
+		"RandomName9",
+		"RandomName10",
+		"RandomName11",
+		"RandomName12",
+		"RandomName13",
+		"RandomName14",
+		"RandomName15"
+		};
+	}
+}
