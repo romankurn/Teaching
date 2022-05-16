02948d6c9c9fd7154aa9a87e2c64d08411f59ec0
romankurn <89409920+romankurn@users.noreply.github.com>
2022-05-16 16:38:21 +0300

    l (#61)

---
+++ 13.Паттерны/Patterns/Patterns/Proxy/Implementation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Proxy
+{
+	public static class Implementation
+	{
+		public static void Execute()
+		{
+			var stopwatch = new Stopwatch();
+
+			stopwatch.Start();
+			var site = new Site();
+
+			var page3 = site.Find("url3");
+
+			var page2 = page3.Click(site, "url2");
+			var page1 = page2.Click(site, "url1");
+			page3 = page1.Click(site, "url3");
+			page2 = page3.Click(site, "url2");
+			stopwatch.Stop();
+
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+			stopwatch.Restart();
+			var siteProxy = new SiteProxy(site);
+			
+			page3 = siteProxy.Find("url3");
+			page2 = page3.Click(siteProxy, "url2");
+			page1 = page2.Click(siteProxy, "url1");
+			page3 = page1.Click(siteProxy, "url3");
+			page2 = page3.Click(siteProxy, "url2");
+			stopwatch.Stop();
+
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+		}
+	}
+}
