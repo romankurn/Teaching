f5280312e06b148e34a938aff0ebaaa419c55fa7
romankurn <89409920+romankurn@users.noreply.github.com>
2022-05-19 19:28:32 +0300

    д (#62)

---
+++ 13.Паттерны/HW/HW/HW/Strategy/Figures/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW.Strategy
+{
+	public interface IFigure
+	{
+		string Name { get; }
+		
+		double CalculateArea();
+	}
+}
