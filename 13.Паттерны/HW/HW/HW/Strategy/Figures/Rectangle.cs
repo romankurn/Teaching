daaf77f3a7dc2d70152edaa0353fe3bfc1cc9fc3
romankurn <89409920+romankurn@users.noreply.github.com>
2022-05-19 19:28:03 +0300

    д

---
+++ 13.Паттерны/HW/HW/HW/Strategy/Figures/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW.Strategy
+{
+	public class Rectangle : IFigure
+	{
+		public int Height { get; private set; }
+
+		public int Width { get; private set; }
+
+		public string Name { get; }
+
+		public Rectangle(int height, int width)
+		{
+			Height = height;
+			Width = width;
+			Name = "Rectangle";
+		}
+
+		public double CalculateArea()
+		{
+			return Height * Width;
+		}
+	}
+}
---
+++ 13.Паттерны/HW/HW/HW/Strategy/Figures/Trapezoid.cs
@@ -0,0 +1,26 @@
+﻿namespace HW.Strategy
+{
+	public class Trapezoid : IFigure
+	{
+		public int BaseA { get; private set; }
+
+		public int BaseB { get; private set; }
+
+		public int Height { get; private set; }
+
+		public string Name { get; }
+
+		public Trapezoid(int baseA, int baseB, int height)
+		{
+			BaseA = baseA;
+			BaseB = baseB;
+			Height = height;
+			Name = "Trapezoid";
+		}
+
+		public double CalculateArea()
+		{
+			return ((BaseA + BaseB) / 2) * Height;
+		}
+	}
+}
