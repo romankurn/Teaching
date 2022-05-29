8d2b9a6657653aa3e5cf9be8bd8fe471bb61f144
romankurn <89409920+romankurn@users.noreply.github.com>
2022-05-29 12:17:10 +0300

    l

---
+++ 14.Клиент-серверное приложение через Socket/клиент/Client/Client/Expression.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+	internal class Expression
+	{
+		public double Value1 { get; set; }
+
+		public double Value2 { get; set; }
+
+		public string Operation { get; set; }
+
+		public Expression(string expression)
+		{
+			var components = expression.Split(' '); // обработка получения строки
+
+			Value1 = double.Parse(components[0]);
+
+			switch (components[1])
+			{ 
+				case "+":
+					Operation = Client.Operation.Addition.ToString();
+					break;
+
+				case "-":
+					Operation = Client.Operation.Subtraction.ToString();
+					break;
+
+				case "*":
+					Operation = Client.Operation.Multiplication.ToString();
+					break;
+
+				case "/":
+					Operation = Client.Operation.Division.ToString();
+					break;
+
+				case "^":
+					Operation = Client.Operation.Pow.ToString();
+					break;
+
+				default:
+					Operation = Client.Operation.Utca.ToString();
+					break;
+			}
+
+			Value2 = double.Parse(components[2]);
+		}
+	}
+}
