c58d9bd5d307a125956e055d46468ca10fbf20b1
romankurn <89409920+romankurn@users.noreply.github.com>
2022-05-31 19:19:13 +0300

    l (#64)

---
+++ 15. Boxing/Boxing/Boxing/MyObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+	internal class MyObject
+	{
+		public int a;
+
+		public override bool Equals(object obj)
+		{
+			if(obj == null)
+				return false;
+
+			return (obj as MyObject)?.a == a;
+		}
+	}
+}
